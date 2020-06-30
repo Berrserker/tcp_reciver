bc077f0e02b8ef8f985b60d9790912daf334541c
Yago_Sevatarion <berrserk.s@gmail.com>
2020-06-30 14:37:33 +0300

    Initial Commit

---
+++ tcp_reciver/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  tcp_reciver
+//
+//  Created by MAC on 30.06.2020.
+//  Copyright © 2020 Yago_Sevatarion. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
